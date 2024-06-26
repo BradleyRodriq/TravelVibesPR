7e38157b465c1713b87041de7ddb7137d4c8285f
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-06-26 11:56:40 -0400

    reroute login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,20 @@
 import { useState } from "react"
 import { useLogin } from "../hooks/useLogin"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {login, isLoading, error} = useLogin()
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        await login(email, password)
+        const success = await login(email, password)
+        if (success) {
+            navigate('/')
+        }
     }
 
     return (
