fd0ec6e40a3c0824dea7a880d3ea4db266257f55
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-07-05 20:40:31 -0400

    deployment fixes

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
 
         // listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('Connected to database and listening on port', process.env.PORT)
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log('Connected to database and listening on port', process.env.SERVER_PORT)
         })
     })
     .catch((error) => {
