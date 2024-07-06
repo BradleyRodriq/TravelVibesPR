005b5106118d8a69fd10bbc34621377a9495651c
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-07-06 10:41:41 -0400

    landing page pictures

--- src/pages/ExperienceMap.js
+++ src/pages/ExperienceMap.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
 import { useNavigate } from "react-router-dom";
+import "../styles/Map.css";
 
 const mapContainerStyle = {
   width: "100%",
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import '../styles/Landing.css';
+import BradleyImage from '../pictures/bradley.jpg';
+import LeandroImage from '../pictures/leandro.jpg';
+/*import SebastianImage from '../pictures/sebastian.jpg'*/
+import VibesImage from '../pictures/vibes.png';
+import MapImage from '../pictures/Map-view.png';
+import ExperienceImage from '../pictures/experiences.png';
 
 function Landing() {
     return (
@@ -11,28 +17,41 @@ function Landing() {
                 <div className="team-container">
                     <h2>Meet our team!</h2>
                     <div className="team-member-container">
-                        <h3 className='team-member'>Bradley A. Rodriguez</h3>
-                        <p>Full-stack Dev</p>
-                        <h3 className='team-member'>Leandro Pagani</h3>
-                        <p>Backend Dev</p>
-                        <h3 className='team-member'>Sebastian Quiñones</h3>
-                        <p>Frontend Dev</p>
+                        <div className="team-member">
+                            <img src={BradleyImage} alt="Bradley A. Rodriguez" />
+                            <h3>Bradley Rodriguez</h3>
+                            <p>Full-stack Dev</p>
+                        </div>
+                        <div className="team-member">
+                            <img src={LeandroImage} alt="Leandro Pagani" />
+                            <h3>Leandro Pagani</h3>
+                            <p>Backend Dev</p>
+                        </div>
+                        <div className="team-member">
+                            <h3>Sebastian Quiñones</h3>
+                            <p>Frontend Dev</p>
+                        </div>
                     </div>
                 </div>
                 <div>
-                    <h1 className='features-header'>Features</h1>
+                    <h1 className="features-header">Features</h1>
                 </div>
                 <div className="feature">
-                    <h2>Vibe Matching</h2>
-                    <p>Match up your vibes with our growing experiences database!</p>
+                <h2>Vibe Matching</h2>
+                <p>Match up your vibes with our growing experiences database!</p>
+                    <img src={VibesImage} alt="Vibes" />
+
                 </div>
                 <div className="feature">
                     <h2>Map View</h2>
                     <p>View our experiences as pins in our seamless map experience!</p>
+                    <img src={MapImage} alt="Map View" />
+
                 </div>
                 <div className="feature">
                     <h2>Experience Creation</h2>
                     <p>Share your experiences with the world!</p>
+                    <img src={ExperienceImage} alt="Experiences" />
                 </div>
                 <div className="feature">
                     <h2>Automatic Notifications</h2>
@@ -41,7 +60,6 @@ function Landing() {
                 <div className="Website-link">
                     <a href="/home" className="home-button">Visit TravelVibesPR!</a>
                 </div>
-
                 <footer className="footer">
                     <p>&copy; 2024 TravelVibesPR. All rights reserved.</p>
                 </footer>
